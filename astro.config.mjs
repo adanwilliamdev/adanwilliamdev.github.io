731048e9723bf37abb9c201eee32844454f06edd
Adan William <adan.william.dev@gmail.com>
2026-07-19 21:37:10 -0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 ﻿import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   site: "https://adanwilliamdev.github.io",
   base: "/",
   output: "static",
